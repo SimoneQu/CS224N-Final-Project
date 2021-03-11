901d1876b764b7b73fcd14e3aeb92da1266feb68
Simone Qu <simengqu@gmail.com>
2021-03-11 16:34:32 -0500

    add maml

---
+++ args.py
@@ -0,0 +1,57 @@
+import argparse
+
+def init_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch-size', type=int, default=32)
+    parser.add_argument('--num-epochs', type=int, default=5)
+    parser.add_argument('--lr', type=float, default=3e-5)
+    parser.add_argument('--num-visuals', type=int, default=10)
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--save-dir', type=str, default='save/')
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--eval', action='store_true')
+    parser.add_argument('--train-datasets', type=str, default='squad,nat_questions,newsqa')
+    parser.add_argument('--run-name', type=str, default='multitask_distilbert')
+    parser.add_argument('--recompute-features', action='store_true')
+    parser.add_argument('--train-dir', type=str, default='datasets/indomain_train')
+    parser.add_argument('--val-dir', type=str, default='datasets/indomain_val')
+    parser.add_argument('--eval-dir', type=str, default='datasets/oodomain_test')
+    parser.add_argument('--eval-datasets', type=str, default='race,relation_extraction,duorc')
+    parser.add_argument('--do-train', action='store_true')
+    parser.add_argument('--do-finetune', action='store_true')
+    parser.add_argument('--do-eval', action='store_true')
+    parser.add_argument('--sub-file', type=str, default='')
+    parser.add_argument('--visualize-predictions', action='store_true')
+    parser.add_argument('--eval-every', type=int, default=2000)
+
+    ## maml specific
+    parser.add_argument('--num-inner-updates', type=int, default=5)
+    parser.add_argument('--num-tasks', type=int, default=8)
+    parser.add_argument('--inner-lr', type=float, default=1e-3)
+    parser.add_argument('--task-weight', type=str, default="proportional")
+    parser.add_argument('--meta-update', type=str, default="reptile")
+    parser.add_argument('--meta-lr', type=float, default=5e-5)
+
+    return parser
+
+def get_train_test_args():
+    parser = init_parser()
+    args = parser.parse_args()
+    return args
+
+def get_debug_args(run_name="maml", meta_update="reptile"):
+    parser = init_parser()
+
+    input = [
+        '--run-name', run_name,
+        '--meta-update', meta_update,
+        '--do-train',
+        '--batch-size', '1',
+        '--eval-every', '2000',
+        '--num-inner-updates', '3',
+        '--num-tasks', '2'
+        #"--recompute-features"
+    ]
+    args = parser.parse_args(input)
+    return args
+
---
+++ subsample.py
@@ -0,0 +1,79 @@
+import json
+import random
+import copy
+import os
+
+
+def subsample(data_file, subsample_num):
+    squad_data = json.load(open(data_file, 'rb'))
+    # Count number of questions
+    all_ids = []
+    for example in squad_data["data"]:
+        for paragraph in example["paragraphs"]:
+            for question_answer in paragraph["qas"]:
+                all_ids.append(question_answer['id'])
+
+    chosen_ids = set(random.sample(all_ids, k=subsample_num))
+    subsampled_data = copy.deepcopy(squad_data)
+    # Verify the data
+    for article in subsampled_data["data"]:
+        for paragraph in article["paragraphs"]:
+            new_qas = []
+            for qas in paragraph["qas"]:
+                if qas['id'] in chosen_ids:
+                    new_qas.append(qas)
+            paragraph['qas'] = new_qas
+
+    with open(data_file + '_subsampled', "w") as output_file:
+        json.dump(subsampled_data, output_file)
+
+def subsample_by_example(folder, file, pct, folder_to_write, file_to_write=None):
+    data_file = os.path.join(folder, file)
+    squad_data = json.load(open(data_file, 'rb'))
+    print(f"Reading in {file}")
+    n = len(squad_data['data'])
+    print("Total count of data:", n)
+
+    # Count number of questions
+    subsample_num = int(n * pct) + 1
+    subsample = random.sample(squad_data['data'], subsample_num)
+    squad_data['data'] = subsample
+
+    if file_to_write is None:
+        file_to_write = file
+    dir_to_write = os.path.join(folder_to_write, file_to_write)
+    print(f"writing {subsample_num} subsamples to {dir_to_write}")
+    with open(dir_to_write, "w") as output_file:
+        json.dump(squad_data, output_file)
+
+def count(data_file):
+    squad_data = json.load(open(data_file, 'rb'))
+    # Count number of questions
+    all_ids = []
+    count1 = 0
+    for example in squad_data["data"]:
+        flag = 0
+        for paragraph in example["paragraphs"]:
+            for question_answer in paragraph["qas"]:
+                flag = 1
+                all_ids.append(question_answer['id'])
+        count1 += flag
+
+    print(data_file)
+    print("total number of examples:", len(squad_data["data"]))
+    print("total number of examples with questions:", count1)
+    print("total number of questions:", len(all_ids))
+
+
+if __name__ == '__main__':
+    random.seed(2021)
+    # folder = os.path.join(os.getcwd(), "datasets", "indomain_train")
+    folder = os.path.join(os.getcwd(), "datasets", "indomain_val")
+    folder_to_write = os.path.join(os.getcwd(), "datasets_subsample", "indomain_val")
+    if not os.path.exists(folder_to_write):
+        os.mkdir(folder_to_write)
+    for filename in ['nat_questions', 'newsqa', 'squad']:
+        # dir = os.path.join(folder, filename)
+        # count(dir)
+        # subsample(dir, 500)
+        subsample_by_example(folder, filename, 0.01, folder_to_write)
